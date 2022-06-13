361eaeb9776d3779f63334ca50332c0f4fadb533
valen.2021 <quepasaporahi@live.com>
2022-06-13 14:15:31 +0200

    FEAT: Css home page

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,10 +1,4 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home </router-link>|
-    <router-link to="/about">About </router-link>|   
-    <router-link to="/alignments">Alinear</router-link>|
-    <router-link to="/sequences/add">Añadir nueva secuencia</router-link>|
-    <router-link to="/sequences/<id>">Editar secuencia</router-link>
-  </div>
+  
   <router-view />
 </template>
--- front/src/pages/home/HomePage.vue
+++ front/src/pages/home/HomePage.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/img/logo.png" />
-    <h1>{{ info.app_name }}</h1>
-  </div>
+  <body>
+    <header>
+      <div class="logo">
+          <img id="header-img" src="@/assets/img/logo.png" alt="adn searcher logo" />
+      </div>
+      <div class="nav">
+        <div class="nav-link">
+          <router-link to="/">Home </router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/about">About </router-link>  
+        </div>
+        <div class="nav-link">
+          <router-link to="/alignments">Alinear</router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/sequences/add">Añadir nueva secuencia</router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/sequences/<id>">Editar secuencia</router-link>
+        </div>
+      </div>
+     
+    </header>
+    <main>
+      <section class="banner">
+          <div class="banner-text">
+            <h1>ADN SEARCHER</h1>
+            <p>La mejor web de consultas de secuencias genéticas</p>
+          </div>
+      </section>
+    </main>
+  </body>
 </template>
 
 <script>
@@ -29,7 +58,54 @@ export default {
 </script>
 
 <style scoped>
-h1 {
-  font-style: italic;
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
 }
+body{
+  background:linear-gradient(rgba(0, 0, 0, 0.356),#009688),url(banner.jpg);
+  background-size: cover;
+  background-position: center;
+  height:100vh;
+  width:100vw;
+  
+}
+header {
+  position: relative;
+  top: 0;
+  min-height: 75px;
+  padding: 0px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.logo > img {
+  max-width: 80px;
+  margin-left: 10px;
+  margin-top: 10px;
+  width:60%;
+}
+.nav{
+  display:flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+.nav-link{
+  margin:10px;
+  margin-right: 10px;
+}
+h1{
+  font-size:9vw;
+  color:whitesmoke;
+}
+.banner{
+  width:68%;
+  height: 50%;
+  margin:auto;
+  padding:8vw;
+  background-color: rgba(240, 248, 255, 0.244);
+  
+}
+
 </style>
