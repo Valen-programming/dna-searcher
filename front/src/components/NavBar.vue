0460d2c53678500822bbdeaf3fe6457bf155d528
valen.2021 <quepasaporahi@live.com>
2022-06-22 18:30:54 +0200

    FIX: Hamburger menu responsive

--- front/src/components/NavBar.vue
+++ front/src/components/NavBar.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="nav-bar">
-     <div class="logo">
+      <div class="logo">
           <img id="header-img" src="@/assets/img/logo.png" alt="adn searcher logo" @click="$router.push('/')"/>
       </div>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <div class="nav">
-        <a href="#home" class="active">..</a>
-
-          <div id="myLinks">
-
+      <input id="toggle" type="checkbox">
+      <label for="toggle" class="hamburger">
+        <div class="top-bun"></div>
+        <div class="meat"></div>
+        <div class="bottom-bun"></div>
+      </label>
+     <div class="nav">
+        <nav>
+          <div class="nav-wrapper">
             <router-link to="/">
               <div class="nav-link">Home</div> 
             </router-link>
@@ -25,7 +27,8 @@
             <router-link to="/categories">
               <div class="nav-link">Catálogo de secuencias</div>
             </router-link>
-      </div>
+          </div>
+        </nav>
         <a href="javascript:void(0);" class="icon" @click="myFunction()">
           <i class="fa fa-bars"></i>
         </a>
@@ -42,16 +45,16 @@
       return {
       }
     },
-    methods:{
-      myFunction(){
-        let x = document.getElementById("myLinks");
-        if (x.style.display === "block") {
-          x.style.display = "none";
-        } else {
-          x.style.display = "block";
-        }
-      }
-    }
+    // methods:{
+    //   myFunction(){
+    //     let x = document.getElementById("nav-wrapper");
+    //     if (x.style.display === "block") {
+    //       x.style.display = "none";
+    //     } else {
+    //       x.style.display = "block";
+    //     }
+    //   }
+    // }
     
   }
 </script>
@@ -79,58 +82,104 @@
   padding: white;
   cursor: pointer;
 }
-#myLinks{
+#toggle {
+  display: none;
+
+}
+.hamburger {
+  position: absolute;
+  top: 5em;
+  right: 5%;
+  margin-left: -2em;
+  margin-top: -45px;
+  width: 2em;
+  height: 45px;
+  z-index: 5;
+  display:none;
+}
+
+.hamburger div {
+  position: relative;
+  width: 3em;
+  height: 7px;
+  border-radius: 3px;
+  background-color: white;
+  margin-top: 8px;
+  border:black solid 1.5px;
+  cursor:pointer;
+  display:none;
+}
+.nav-wrapper{
   display:flex;
   flex-direction: row;
   background-color: rgba(240, 248, 255, 0.838);
   border-radius:3px;
 }
-#myLinks .nav-link{
+.nav-wrapper .nav-link{
   margin:5px;
   padding:15px;
   font-size:100%;
   border-radius: 3px;
   
 }
-#myLinks .nav-link:hover {
+.nav-wrapper .nav-link:hover {
   background-color: rgba(255, 255, 255, 0.841);
   transition: background-color 0.8s;
   box-shadow: 0px 0px 5px rgb(118, 133, 207);
 }
-.nav a.icon{
-  display:none;
-}
-@media(max-width:800px){
+
+@media(max-width:750px){
+  .hamburger {
+    display:block;
+  }
+  .hamburger div {
+    display:block;
+  }
   .nav {
+    position: fixed;
+    width: 70%;
+    height: 40%;
+    background-color: #090E6B;
+    top: 0;
+    right: 0;
     overflow: hidden;
-    background-color: rgb(70, 133, 161);
+    transition: all 0.3s ease-in-out;
+    transform: scale(0);
+    z-index: 4;
+  }
+  .nav-wrapper {
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    overflow-y: auto;
+    height: 100%;
+  }
+  .nav-wrapper .nav-link:hover {
+    background-color: rgba(255, 255, 255, 0.841);
+    transition: background-color 0.8s;
+    box-shadow: 0px 0px 5px rgb(118, 133, 207);
+}
+  nav {
+    text-align: left;
+    margin-left: 10%;
+  }
+  nav-link {
     position: relative;
-  }
-  .nav #myLinks {
-    display: none;
-  }
-  .nav .nav-link {
-    color: white;
-    padding: 14px 16px;
     text-decoration: none;
-    font-size: 17px;
-    display: block;
+    color: #6ecddd;
+    font-size: 1.2em;
+    display: inline-block;
+    margin-top: 1.25em;
+    transition: color 0.2s ease-in-out;
+    letter-spacing: 1px;
   }
-  .nav a.icon {
-    background: rgb(0, 0, 0);
-    display: block;
-    position: absolute;
-    right: 3px;
-    top: 2px;
-  }
-  .nav .nav-link:hover {
-    background-color: #ddd;
-    color: black;
-  }
-  .active {
-    background-color: rgb(70, 133, 161);
-    color: white;
-  }
+}
+
+
+#toggle:checked + .hamburger + .nav {
+  top: 0;
+  transform: scale(1);
+  
 }
 
 </style>
